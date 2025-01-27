548a2fe40e21b34ea0e4867d247d4a39286a18ea
asserelsheikh <139915539+asserelsheikh@users.noreply.github.com>
2025-01-27 03:21:04 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,149 @@
+// Project Data
+const projects = [
+    {
+        title: "Churn Analysis for Student Retention",
+        description: "Developed predictive models using Decision Tree and Logistic Regression to forecast student churn risk, achieving 85% accuracy in predictions.",
+        link: "https://drive.google.com/file/d/1j8rq2zpytgJnmIQcAuIQIOk8hJsd1EP7/view?usp=drive_link",
+        tags: ["#Python ", "#Machine Learning ", "#Data Analysis "]
+    },
+    {
+        title: "POS Stock Control Database",
+        description: "Designed and implemented an optimized database system for inventory management and spending analysis, improving efficiency by 40%.",
+        link: "https://github.com/asserelsheikh/SQLproject",
+        tags: ["#SQL ", "#Database Design ", "#Analytics "]
+    },
+    {
+        title: "Sales Performance and Forecasting Tool",
+        description: "Developed an interactive Streamlit application to analyze sales data, providing insights through custom visualizations, customer segmentation, and automated PDF reporting. Achieving high accuracy in forecasting daily and hourly sales trends.",
+        link: "https://github.com/asserelsheikh/salesforcastingapp",
+        tags: ["#Python ", "#Streamlit ", "#Data Analysis ", "#Machine Learning "]
+    },    
+    {
+        title: "Sales Performance Dashboard",
+        description: "Created an interactive dashboard using Looker Studio to visualize sales trends and KPIs, leading to a 25% increase in sales team productivity.",
+        link: "https://drive.google.com/file/d/10gHzIj8l8d7QFLjjzPzjH7es3U7vm45E/view?usp=drive_link",
+        tags: ["#Looker Studio ", "#Data Visualization ", "#Business Intelligence "]
+    },
+    {
+        title: "Demographic Data Analysis with Adult Dataset",
+        description: "Analyzed demographic factors such as education, race, and income using Python to extract insights, such as percentage distributions and correlations between variables.",
+        link: "https://drive.google.com/file/d/1oEizrirZjbG8W13o70W8miUaQYDwYWdb/view?usp=drive_link",
+        tags: ["#Python ", "#Data Analysis ", "#Pandas "]
+    },
+    {
+        title: "Sales Insights for Quantium Internship",
+        description: "Developed visualizations and statistical analysis for sales trends, customer behaviors, and product performance using Python, resulting in actionable insights for marketing strategies.",
+        link: "https://drive.google.com/file/d/1yb65MadlkAQDIVksfTEW_pugBU2cBqiE/view?usp=drive_link",
+        tags: ["#Python ", "#Data Visualization ", "#Customer Insights "]
+    }
+    
+];
+
+// Certificate Data
+const certificates = [
+    {
+        title: "AI Career Essentials",
+        issuer: "ALX",
+        date: "July 2024",
+        verify: "https://intranet.alxswe.com/certificates/fZMHNFLn3r",
+        icon: "fas fa-robot"
+    },
+    {
+        title: "Elements of AI",
+        issuer: "University of Helsinki",
+        date: "April 2024",
+        verify: "https://certificates.mooc.fi/validate/12xhtn0q2e",
+        icon: "fas fa-brain"
+    },
+    {
+        title: "Data Analysis with Python",
+        issuer: "freeCodeCamp",
+        date: "September 2022",
+        verify: "https://freecodecamp.org/certification/Asser_Elsheikh/data-analysis-with-python-v7",
+        icon: "fab fa-python"
+    },
+    {
+        title: "CS50's Introduction to Databases with SQL",
+        issuer: "Harvard University",
+        date: "May 2024",
+        verify: "https://cs50.harvard.edu/certificates/c75faba5-2909-4fc5-a41c-4bfe1d2e4810",
+        icon: "fas fa-database"
+    },
+    {
+        title: "Applied Data Science with Python",
+        issuer: "IBM",
+        date: "July 2024",
+        verify: "https://www.credly.com/go/C6JHZpOX",
+        icon: "fas fa-chart-line"
+    },
+    {
+        title: "CS50's Introduction to programming with Python",
+        issuer: "Harvard University",
+        date: "December 2024",
+        verify: "https://cs50.harvard.edu/certificates/91d50c0a-4c3a-4c5f-994c-3dec8545e17a",
+        icon: "fas fa-terminal"
+    },
+    {
+        title: "Data analysis with R programming",
+        issuer: "Coursera - Google",
+        date: "September 2023",
+        verify: "https://coursera.org/verify/V7EH4SWKDM7N",
+        icon: "fab fa-google"
+    },
+    {
+        title: "Inequality analysis using R: Disaggregated data from surveys",
+        issuer: "World Health Organization",
+        date: "August 2023",
+        verify: "https://openwho.org/verify/xuzem-sipuh-coses-bipiz-vinir",
+        icon: "fas fa-capsules"
+    },
+    {
+        title: " Risk Management and Maturity",
+        issuer: "UNICEF",
+        date: "September 2022",
+        verify: "https://drive.google.com/file/d/1nvOy5DcM2P2n76uy4fI58XNO9khamGwt/view?usp=drive_link",
+        icon: "fas fa-suitcase"
+    },
+    {
+        title: "Data analytics Essentials",
+        issuer: "CISCO Networking",
+        date: "June 2024",
+        verify: "https://www.credly.com/badges/dc1ca157-055f-4307-9d15-1182e7b795b0/print",
+        icon: "fas fa-chart-pie"
+    },
+    {
+        title: "Ask Questions to Make Data-Driven Decisions",
+        issuer: "Coursera - Google",
+        date: "September 2022",
+        verify: "https://coursera.org/verify/DXBWDKYSZZL5",
+        icon: "fab fa-google"
+    },
+    {
+        title: "Foundations: Data, Data, Everywhere",
+        issuer: "Coursera - Google",
+        date: "September 2022",
+        verify: "https://coursera.org/verify/SQ5L7C4G473J",
+        icon: "fab fa-google"
+    },
+    {
+        title: "Prepare Data for Exploration",
+        issuer: "Coursera - Google",
+        date: "September 2022",
+        verify: "https://coursera.org/verify/Y4T7VRCPGV37",
+        icon: "fab fa-google"
+    },
+    {
+        title: "Prompt Engineering",
+        issuer: "1 Million prompter intiative - Future Dubai Foundation",
+        date: "Janaury 2025",
+        verify: "https://omp.dub.ai/certificate/qUYxI3031jp7",
+        icon: "fas fa-robot"
+    },
+    {
+        title: "Process data from dirty to clean",
+        issuer: "Coursera - Google",
+        date: "Janaury 2025",
+        verify: "https://coursera.org/verify/SFKV3ZXXR16W",
+        icon: "fab fa-google"
+    }
+];
